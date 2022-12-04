e9fddcb78f1ce4b6f63409aeab0acfacfaef5df7
JinLisek <tastyramen@gmail.com>
2022-12-04 23:17:35 +0100

    Change RegionsRoot into functional component

--- src/components/RegionsRoot.tsx
+++ src/components/RegionsRoot.tsx
@@ -1,27 +1,22 @@
 import axios from "axios";
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 import RegionsList from "./RegionsList";
 import RegionApiData from "./RegionApiData";
-
-interface RegionsState {
-  regions: string[];
-}
 
 const getRegions = async () => {
   let resp = await axios.get("http://pokeapi.co/api/v2/region/");
   return resp.data.results.map((region: RegionApiData) => region.name);
 };
 
-class RegionsRoot extends Component {
-  state: RegionsState = {
-    regions: [],
-  };
+const RegionsRoot = () => {
+  const [regions, setRegions] = useState<string[]>([]);
+  console.log(regions);
 
-  componentDidMount = async () => {
-    getRegions().then((regions) => this.setState({ regions }));
-  };
+  useEffect(() => {
+    getRegions().then((newRegions) => setRegions(newRegions));
+  }, []);
 
-  render = () => <RegionsList regions={this.state.regions} />;
-}
+  return <RegionsList regions={regions} />;
+};
 
 export default RegionsRoot;
